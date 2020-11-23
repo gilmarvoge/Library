987d267082f59e563091dff1fed1fe40be7fa1b2
gilmarvogel <gilmarvogel@gmail.com>
2020-11-23 08:26:01 -0300

    test

--- src/pages/Login/index.test.js
+++ src/pages/Login/index.test.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Enzyme from 'enzyme';
 import { users } from 'services/mocks';
 import { Provider, LocalizeProvider } from 'react-redux';
+import { Router } from 'react-router-dom';
 import store from 'store';
 import renderer from 'react-test-renderer';
+import { createMemoryHistory } from 'history'
 import Login from 'pages/Login';
 import { render, fireEvent, screen } from '../../utils/test.utils';
 
@@ -14,32 +17,36 @@ import { render, fireEvent, screen } from '../../utils/test.utils';
 
 describe("<Login />", () => {
   it("renders text input correctly", () => {
-    jest.mock('react-router-dom', () => ({
-      useHistory: () => ({
-        push: jest.fn(),
-      }),
-    }));
+    // const historyMock  =jest.mock('react-router-dom', () => ({
+    //   ...jest.requireActual('react-router-dom'),
+    //   useHistory: () => ({
+    //     push: jest.fn()
+    //   })
+    // }));
+    const historyMock = { push: jest.fn(), location: {}, listen: jest.fn() };
     const component  = renderer.create(   
-        <Provider store={store}>
-          <Login />
+        <Provider store={store}>    
+        <Router history={historyMock}>  
+          <Login />  
+          </Router>         
         </Provider>
     )
        let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  // it("calls onLogin when button clicked", () => {
-  //   const onSubmitMock = jest.fn();
+  it("calls onLogin when button clicked", () => {
+    const onSubmitMock = jest.fn();
 
-  //   const component = Enzyme.mount(
-  //     <Login onSubmit={onSubmitMock} />
-  //   );
+    const component = Enzyme.mount(
+      <Login onSubmit={onSubmitMock} />
+    );
 
-  //   component.find("input.user").simulate('change', { target: { value: users[0].user } })
-  //   component.find("input.password").simulate('change', { target: { value: users[0].password } })
-  //   component.find("form").simulate("submit");
+    component.find("input.user").simulate('change', { target: { value: users[0].user } })
+    component.find("input.password").simulate('change', { target: { value: users[0].password } })
+    component.find("form").simulate("submit");
 
-  //   console.log("onClickMock.mock", onSubmitMock.mock)
-  //   expect(onSubmitMock).toBeCalled()
-  // });
+    console.log("onClickMock.mock", onSubmitMock.mock)
+    expect(onSubmitMock).toBeCalled()
+  });
 });
